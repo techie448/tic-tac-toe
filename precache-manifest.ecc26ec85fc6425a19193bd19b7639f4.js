74b741658fee0e91ff4050bf87f8862e72c42b67
rishabhkharbanda <techie448@gmail.com>
2020-03-19 21:00:49 -0400

    Updates

---
+++ precache-manifest.ecc26ec85fc6425a19193bd19b7639f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b63d9922df23569bb02571181f67b7",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "be5d143418ee9db47197",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "244cc5651cd6a964d55a",
+    "url": "/tic-tac-toe/static/js/2.661c19bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.661c19bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5d143418ee9db47197",
+    "url": "/tic-tac-toe/static/js/main.7c55ab98.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
